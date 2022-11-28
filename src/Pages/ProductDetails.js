6e4a63dbb181c3a71c5ea10558bebdaaf3be71da
Tsering Dorjee Lama <villadorje29@gmail.com>
2022-11-28 21:07:28 +0545

    button in product detail has been added

--- src/Pages/ProductDetails.js
+++ src/Pages/ProductDetails.js
@@ -8,6 +8,16 @@ export default function ProductDetails() {
     let {productId} = useParams();
     
     let [product, setProduct] = useState({});
+    
+    let [count, setCount] = useState(0);
+
+    function decrementCount() {
+      setCount(prevCount => prevCount -1)
+    }
+
+    function incrementCount() {
+      setCount(prevCount => prevCount + 1)
+    }
 
     useEffect(() => {
         axios.get(`https://fakestoreapi.com/products/${productId}`)
@@ -23,12 +33,19 @@ export default function ProductDetails() {
         <Shop />
         <div className="container py-5  ">
             <div className="row">
-              <div className="col-lg-5">
+              <div className="col-lg-5 pe-5">
+                <div className="card  p-5">
                 <img src={product.image} alt="" className='w-100' />
+                </div>
               </div>
-              <div className="col-lg-7">
+              <div className="col-lg-7 ps-4">
                 <h2>{product.title}</h2>
-                <p><i class="bi bi-star-fill"></i><i class="bi bi-star-fill"></i><i class="bi bi-star-fill"></i><i class="bi bi-star-fill"></i><i class="bi bi-star-fill"></i><Link>(1 customer review)</Link></p>
+                <p><i class="bi bi-star-fill"></i><i class="bi bi-star-fill"></i><i class="bi bi-star-fill"></i><i class="bi bi-star-fill"></i><i class="bi bi-star-fill"></i><Link to="" className="text-decoration-none text-dark mx-2">(1 customer review)</Link></p>
+                <p className='fw-bolder'>$ {product.price}</p>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere unde aperiam velit odit consectetur eos quis vero? Quibusdam placeat quasi eum cum temporibus qui repellat error, facilis eveniet dolorum nihil, sapiente accusamus ipsa illo.</p>
+                <div className='fs-4 ms-4 border border-dark d-inline-block py-1 px-3'>{count}</div>
+                <button onClick={incrementCount} className="py-1 px-3 fs-5">+</button>
+                <button onClick={decrementCount} className="py-1 px-3 fs-5">-</button>
               </div>
             </div>
         </div>
