56e8e4d4c441ffe6a7a9ff05fdb57be12340bca2
Tsering Dorjee Lama <villadorje29@gmail.com>
2023-01-01 16:12:21 +0545

    search button is added to Header.js

--- src/Components/form/Header.js
+++ src/Components/form/Header.js
@@ -1,7 +1,16 @@
-import React from 'react'
+import React, {useState} from 'react'
 import { Link } from 'react-router-dom'
 
 export default function Header() {
+    const [visible, setVisible] = useState(true);
+    
+
+    const handleVisibility = () => {
+        setVisible(prev => !prev);
+      };
+    
+      
+
   return (
     <>
         <div className="top py-2 ">
@@ -18,11 +27,20 @@ export default function Header() {
                         <ul>
                             <li><Link to={"/track"}>TRACK MY ORDER</Link></li>
                             <li className='border-end border-start'><Link to={"/privacy"}>TERM OF USE</Link></li>
-                            <li><a href='https://goo.gl/maps/pp3rR7kthXBMzeGm6'>OUR LOCATION</a></li>
+                            <li><a href='https://goo.gl/maps/pp3rR7kthXBMzeGm6' target={"_blank"}>OUR LOCATION</a></li>
+                            <button className="btn btn-outline-danger" type="submit" onClick={() => handleVisibility()}>Search</button>
                         </ul>
                     </div>
                 </div>
             </div>
+            <aside className={visible ? "close" : "active"}>
+            <form id='search_button' className="py-5 px-5 bg-white search_header " role="search">
+                  <input className="form-control me-2 bg-light py-3" type="search" placeholder="Search Product" aria-label="Search" />
+                  <button className="btn btn-outline-success" type="submit">Search</button>
+              </form>
+            </aside>
+              
+              
         </div>
     </>
   )
